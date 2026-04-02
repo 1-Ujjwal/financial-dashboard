fc6df227fd962ac6bc904ed569e1b9ffe2c53baf
1-Ujjwal <kingayushman7@gmail.com>
2026-04-02 11:05:57 +0530

    Added Toast Message

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Toast from "./components/Toast";
 
 const App = () => {
-  return <div className="text-3xl text-blue-500">App</div>;
+  return (
+    <div className="min-h-screen bg-linear-to-br from-slate-900 via-purple-900 to-slate-900 p-4">
+      <Toast />
+    </div>
+  );
 };
 
 export default App;
