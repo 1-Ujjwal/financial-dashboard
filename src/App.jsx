13152c2bd479369e3c25bc8875830571d8c942ad
1-Ujjwal <kingayushman7@gmail.com>
2026-04-03 10:52:33 +0530

    Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Toast from "./components/Toast";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <div className="min-h-screen bg-linear-to-br from-slate-900 via-purple-900 to-slate-900 p-4">
-      <Toast />
+      {/* <Toast /> */}
+
+      <div className="max-w-7xl mx-auto">
+        <Header />
+      </div>
     </div>
   );
 };
