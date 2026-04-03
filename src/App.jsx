e7be14902da73d6a31547fc67628cd6de101cf32
1-Ujjwal <kingayushman7@gmail.com>
2026-04-03 11:21:28 +0530

    SummaryCard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import Toast from "./components/Toast";
 import Header from "./components/Header";
+import SummaryCard from "./components/SummaryCard";
 
 const App = () => {
   return (
     <div className="min-h-screen bg-linear-to-br from-slate-900 via-purple-900 to-slate-900 p-4">
       {/* <Toast /> */}
 
+      {/* Header */}
       <div className="max-w-7xl mx-auto">
         <Header />
       </div>
+
+      {/* SummaryCard */}
+      <SummaryCard />
     </div>
   );
 };
