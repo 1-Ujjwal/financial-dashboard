1e230e5c3ca56426a785c87870b16b98a204a945
1-Ujjwal <kingayushman7@gmail.com>
2026-04-04 16:43:43 +0530

    Added Usestate

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,29 @@ import FilterTab from "./components/FilterTab";
 import ExpensesList from "./components/ExpensesList";
 
 const App = () => {
+  const [editingId, setIsEditingId] = useState(null);
+  const [expenses, setExpenses] = useState([]);
+  const [filter, setFilter] = useState("all");
+  const [toast, setToasts] = useState([]);
+
+  // Toast Notification Logic
+  const showToast = (message, type = "success") => {
+    const id = Date.now();
+    const toast = { id, message, type };
+    setToasts((prev) => [...prev, toast]);
+
+    setTimeout(() => {
+      setToasts((prev) => prev.filter((t) => t.id !== id));
+    }, 4000);
+  };
+
+  const RemoveToast = (id) => {
+    setToasts((prev) => prev.filter((t) => t.id !== id));
+  };
+
   return (
     <div className="min-h-screen bg-linear-to-br from-slate-900 via-purple-900 to-slate-900 p-4">
-      {/* <Toast /> */}
+      <Toast Toast={toast} RemoveToast={RemoveToast} />
 
       {/* Header */}
       <div className="max-w-7xl mx-auto">
--- src/components/Toast.jsx
+++ src/components/Toast.jsx
@@ -1,26 +1,45 @@
 import { AlertCircle, Check, X } from "lucide-react";
 import React from "react";
 
-const Toast = () => {
-  return (
-    <div className="fixed top-0 right-4 z-50 space-y-2">
-      <div
-        className={`flex items-center p-4 rounded-lg shadow-lg backdrop-blur-sm border transform transition-all duration-300 easy-in-out`}
-      >
-        {/*Conditional Rendering */}
-        <Check className="w-5 h-5 mr-2 shrink-0" />
+const Toast = ({ Toast, RemoveToast }) => {
+  <div className="fixed top-0 right-4 z-50 space-y-2">
+    {Toast.map((t) => {
+      return (
+        <div
+          key={t.id}
+          className={`flex items-center p-4 rounded-lg shadow-lg backdrop-blur-sm border transform transition-all duration-300 easy-in-out ${
+            Toast.type === "success"
+              ? "bg-green-900/80 border-green-700 text-green-100"
+              : Toast.type === "error"
+                ? "bg-red-900/80 border-red-700 text-red-100"
+                : "bg-yellow-900/80 border-yellow-700 text-yellow-100"
+          }`}
+        >
+          {/*Conditional Rendering */}
 
-        <AlertCircle className="w-5 h-5 mr-2 shrink-0" />
+          {Toast.type === "success" && (
+            <Check className="w-5 h-5 mr-2 shrink-0" />
+          )}
 
-        <AlertCircle className="w-5 h-5 mr-2 shrink-0" />
+          {Toast.type === "error" && (
+            <AlertCircle className="w-5 h-5 mr-2 shrink-0" />
+          )}
 
-        <span className="mr-2">Toast Message</span>
-        <button className="ml-auto text-gray-300 hover:text-white transition-colors">
-          <X className="w-4 h-4" />
-        </button>
-      </div>
-    </div>
-  );
+          {Toast.type === "info" && (
+            <AlertCircle className="w-5 h-5 mr-2 shrink-0" />
+          )}
+
+          <span className="mr-2">Toast Message</span>
+          <button
+            className="ml-auto text-gray-300 hover:text-white transition-colors"
+            onClick={() => RemoveToast(t.id)}
+          >
+            <X className="w-4 h-4" />
+          </button>
+        </div>
+      );
+    })}
+  </div>;
 };
 
 export default Toast;
